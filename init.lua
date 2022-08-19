38bd74349784b3ee8de7f9cf90c30f527b467559
mrxiaozhuox <mrxzx.info@gmail.com>
2022-08-19 13:51:40 -0700

    feat: commit code

--- init.lua
+++ init.lua
@@ -4,22 +4,30 @@ package.path = "/Users/mrxzx/Development/DioxusLabs/plugin-dev/plugin-library/li
 local plugin = require("plugin")
 local manager = require("manager")
 
+-- deconstruct api functions
+local log = plugin.log
+
+-- plugin information
 manager.name = "Hello Dixous Plugin"
 manager.repository = "https://github.com/mrxiaozhuox/hello-dioxus-plugin"
 manager.author = "YuKun Liu <mrxzx.info@gmail.com>"
 manager.version = "0.0.1"
 
-
 -- init manager info to plugin api
 plugin.init(manager)
 
 manager.on_init = function ()
-    plugin.log.info("Start to init")
+    log.info("Start to init")
 end
 
----@param info BuildStartInfo
+---@param info BuildInfo
 manager.build.on_start = function (info)
-    plugin.log.info("Build start: " .. info.name)
+    log.info("Build starting: " .. info.name)
+end
+
+---@param info BuildInfo
+manager.build.on_finish = function (info)
+    log.info("build finished: " .. info.name)
 end
 
 return manager
